a388c4f5afee0966df7b5b50844c9fad4746b204
Yash Shukla <yshukla2775@gmail.com>
2026-05-08 00:42:50 +0530

    Initialised frontend for NewsNest project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex text-2xl items-center justify-center bg-black text-red-500 text-4xl">
+      Tailwind Working 🚀
+    </div>
+  )
+}
+
+export default App
